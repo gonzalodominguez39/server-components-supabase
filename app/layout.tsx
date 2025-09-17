99f1c46e706b33bce7652e3a39e304a3999a9024
gonzalodominguez39 <gonzaloemanuel39@gmail.com>
2025-09-17 13:47:12 -0300

    session base de datos- form

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
+import Link from "next/link";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -26,14 +27,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.className} antialiased`}>
+      <body className={`${geistSans.className} bg-black antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
+  <main className="min-h-screen ex flex-col ">
+      <div className="flex-1 w-full flex flex-col gap-20 ">
+        <nav className="w-full flex bg-black justify-center border-b border-b-foreground/10 h-16">
+          <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+            <div className="flex gap-5 items-center font-semibold">
+              <Link className="text-white " href={"/"}>Primer CRUD a supabase</Link>
+            </div>
+          </div>
+        </nav>
+        <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           {children}
+        </div>
+      </div>
+    </main>
         </ThemeProvider>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,104 @@
+"use client";
 
-import { FirstComponent } from '@/components/FirstComponent'
+import { useState } from "react";
+
 export default function Home() {
-  
+    const initialState = {
+    id: -1,
+    first_name: "",
+    last_name: "",
+    age: 0,
+  };
+
+  const [formValues,setFormValues] = useState(initialState)
+  type user = {
+    id: number;
+    first_name: string;
+    last_name: string;
+    age: number;
+    created_at: string;
+  };
+
+
+  const handleFormChange=(e:React.ChangeEvent<HTMLInputElement>)=>{
+    const {name,value}=e.target
+    setFormValues({...formValues,[name]:value})
+  }
+
+
   return (
-    <main>
-      <FirstComponent />
+    <main className="bg-gray-600">
+      <h4 className="text-xl">Lista de usuarios</h4>z
+      <h4 className="text-xl font-bold">Crear Usuario</h4>
+      <form className="mt-4">
+        <div className="flex flex-col gap-4">
+          <div>
+            <label
+              htmlFor="nombre"
+              className="block text-sm font-medium text-gray-300"
+            >
+              Nombre
+            </label>
+            <input
+              type="text"
+              id="nombre"
+              name="nombre"
+              value={formValues.first_name}
+              onChange={handleFormChange}
+              className="mt-1 block w-full px-3 py-2 bg-gray-800 border border-gray-600 rounded-md text-sm shadow-sm placeholder-gray-400
+              focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="apellido"
+              className="block text-sm font-medium text-gray-300"
+            >
+              Apellido
+            </label>
+            <input
+              type="text"
+              id="apellido"
+              name="apellido"
+              value={formValues.last_name}
+              onChange={handleFormChange}
+              className="mt-1 block w-full px-3 py-2 bg-gray-800 border border-gray-600 rounded-md text-sm shadow-sm placeholder-gray-400
+              focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="edad"
+              className="block text-sm font-medium text-gray-300"
+            >
+              Edad
+            </label>
+            <input
+              type="number"
+              id="edad"
+              name="edad"
+              value={formValues.age}
+              onChange={handleFormChange}
+              className="mt-1 block w-full px-3 py-2 bg-gray-800 border border-gray-600 rounded-md text-sm shadow-sm placeholder-gray-400
+              focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
+            />
+          </div>
+        </div>
+        <div className=" flex m-5 justify-center gap-5">
+          <button
+            type="submit"
+            className="mt-6 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-sky-600 hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
+          >
+            Guardar
+          </button>
+          <button
+            type="submit"
+            className="mt-6 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-sky-600 hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
+          >
+            Eliminar
+          </button>
+        </div>
+      </form>
     </main>
   );
 }
